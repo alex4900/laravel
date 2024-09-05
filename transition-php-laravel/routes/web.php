4b38e9eb84b3485cbc29ad76cf95ec054c47f9b4
Alexandre Allain <alexandre.allain@gmail.com>
2024-09-05 11:55:36 +0200

    first commit

---
+++ transition-php-laravel/routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome', ['titre' => 'Mon premier exemple.']);
+});
+Route::get('/ping', function () {
+    return "pong";
+});

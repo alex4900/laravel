757a2579f4060ef2e2a1dfe5cf356450948bbfc8
Alexandre Allain <alexandre.allain@gmail.com>
2024-09-12 10:03:55 +0200

    avance jusqua bdd

--- transition-php-laravel/routes/web.php
+++ transition-php-laravel/routes/web.php
@@ -1,10 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PingPongControleur;
+use App\Http\Controllers\TestFlashController;
+
 
 Route::get('/', function () {
     return view('welcome', ['titre' => 'Mon premier exemple.']);
 });
-Route::get('/ping', function () {
-    return "pong";
-});
+Route::get('/ping', [PingPongControleur::class, 'ping']);
+
+Route::get('/pong', [PingPongControleur::class, 'pong']);
+Route::get('/flash', [TestFlashController::class, 'main']);
+Route::post('/traitement', [TestFlashController::class, 'traitement']);
